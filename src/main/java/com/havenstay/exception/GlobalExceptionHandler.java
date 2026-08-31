c9133511a75d24e3a08eeed6b08162f1bf864905
Anmol-Repo <anmolrana2708@gmail.com>
2026-08-31 18:43:34 +0530

    added complex queries for new features

---
+++ src/main/java/com/havenstay/exception/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.havenstay.exception;
+
+import com.havenstay.dto.ResponseDTO;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDTO> handleAllUnknowExceptions(Exception ex) {
+        ResponseDTO response = ResponseDTO.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ResponseEntity<ResponseDTO> handleNotFoundException(NotFoundException ex) {
+        ResponseDTO response = ResponseDTO.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(NameValueRequiredException.class)
+    public ResponseEntity<ResponseDTO> handleNameValueRequiredException(NameValueRequiredException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidCredentialException.class)
+    public ResponseEntity<Response> handleInvalidCredentialException(InvalidCredentialException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidBookingStateAndDateException.class)
+    public ResponseEntity<Response> handleInvalidBookingStateAndDateException(InvalidBookingStateAndDateException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/havenstay/repository/BookingReferenceRepository.java
+++ src/main/java/com/havenstay/repository/BookingReferenceRepository.java
@@ -3,9 +3,26 @@ package com.havenstay.repository;
 import com.havenstay.entity.Booking;
 import com.havenstay.entity.BookingReference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface BookingReferenceRepository extends JpaRepository<BookingReference, Long> {
     Optional<BookingReference> findByReferenceNo(String referenceNo);
+
+    @Query("""
+           select CASE
+           when  count(b) = 0  then true
+           else false
+           end from Booking b 
+           where b.room.id = :roomId
+           AND   b.checkInDate<= :checkInDate
+           AND   b.checkOutDate>= :checkOutDate
+""")
+    boolean isRoomAvailable(@Param("roomId") Long roomId,
+                            @Param("checkInDate") LocalDate checkInDate,
+                            @Param("checkOutDate") LocalDate checkOutDate
+                             );
 }
