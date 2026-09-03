a5f3775437691c12671025a9a4516bc64a8eb9d5
Anmol-Repo <anmolrana2708@gmail.com>
2026-09-03 23:56:29 +0530

    updated env & tested rest apis

---
+++ src/main/java/com/havenstay/service/RoomService.java
@@ -0,0 +1,23 @@
+package com.havenstay.service;
+
+import com.havenstay.dto.ResponseDTO;
+import com.havenstay.dto.RoomDTO;
+import com.havenstay.enums.RoomType;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.util.List;
+
+    public interface RoomService {
+
+        ResponseDTO addRoom(RoomDTO roomDTO, MultipartFile imageFile);
+        ResponseDTO updateRoom(RoomDTO roomDTO, MultipartFile imageFile);
+        ResponseDTO getAllRooms();
+        ResponseDTO getRoomById(Long id);
+        ResponseDTO deleteRoom(Long id);
+        ResponseDTO getAvailableRooms(LocalDate checkInDate, LocalDate checkOutDate, RoomType roomType);
+        List<RoomType> getAllRoomTypes();
+        ResponseDTO searchRoom(String input);
+    }
+
+}
