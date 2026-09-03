6df199d473f744880246e282c7c40eb7bb18cb93
Anmol-Repo <anmolrana2708@gmail.com>
2026-09-03 22:42:08 +0530

    Add controller classes and test them

---
+++ src/main/java/com/havenstay/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.havenstay.controller;
+
+import com.havenstay.dto.LoginRequestDTO;
+import com.havenstay.dto.RegistrationRequestDTO;
+import com.havenstay.dto.ResponseDTO;
+import com.havenstay.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseDTO> registerUser(@RequestBody @Valid RegistrationRequestDTO request){
+        return ResponseEntity.ok(userService.registerUser(request)) ;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDTO> loginUser(@RequestBody @Valid LoginRequestDTO request){
+        return ResponseEntity.ok(userService.loginUser(request));
+    }
+
+}
---
+++ src/main/java/com/havenstay/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.havenstay.controller;
+
+import com.havenstay.dto.ResponseDTO;
+import com.havenstay.dto.UserDTO;
+import com.havenstay.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<ResponseDTO> getAllUser(){
+        return ResponseEntity.ok(userService.getAllUser());
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDTO> updateOwnAccount(@RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.updateOwnAccount(userDTO));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDTO> deleteOwnAccount(){
+
+        return ResponseEntity.ok(userService.deleteOwnAccount());
+    }
+
+    @GetMapping("/account")
+    public ResponseEntity<ResponseDTO> getOwnAccountDetails(){
+        return ResponseEntity.ok(userService.getOwnAccountDetails());
+    }
+
+
+    @GetMapping("/bookings")
+    public ResponseEntity<ResponseDTO> getMyBookingHistory(){
+        return ResponseEntity.ok(userService.getMyBookingHistory());
+    }
+
+
+
+
+}
